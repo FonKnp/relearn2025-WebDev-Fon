578b4e1030e2e7b8f4a970f6cbfada2ed656b924
FonKnp <fonnfonk@gmail.com>
2025-02-03 16:21:32 -0500

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import { createRoot } from "react-dom";
 import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Pizza Explorer</h1>
-      <Pizza
-        name="The Mediterranean Pizza"
-        description="Spinach, Artichokes, Kalamata Olives, Sun-dried Tomatoes, Feta Cheese, Plum Tomatoes, Red Onions"
-        image={"/public/pizzas/mediterraneo.webp"}
-      />
-      <Pizza
-        name="The Mexicana Pizza"
-        description="Tomatoes, Red Peppers, Jalapeno Peppers, Red Onions, Cilantro, Corn, Chipotle Sauce, Garlic"
-        image={"/public/pizzas/mexicana.webp"}
-      />
-      <Pizza
-        name="The Spinach Pesto Pizza"
-        description="Spinach, Artichokes, Tomatoes, Sun-dried Tomatoes, Garlic, Pesto Sauce"
-        image={"/public/pizzas/spin_pesto.webp"}
-      />
+      <Order />
     </div>
   );
 };
